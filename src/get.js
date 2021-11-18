50e90ecdbafa8246b3a95521d99afaef223d974a
Wouter <wintvelt@me.com>
2021-11-18 22:03:45 +0100

    reintroduce middy

--- src/get.js
+++ src/get.js
@@ -1,14 +1,14 @@
 // handler for GET route
 import Sentry from "@sentry/serverless"
-// import middy from '@middy/core'
-// import errorLogger from '@middy/error-logger'
-// import httpErrorHandler from '@middy/http-error-handler'
-// import cors from '@middy/http-cors'
+import middy from '@middy/core'
+import errorLogger from '@middy/error-logger'
+import httpErrorHandler from '@middy/http-error-handler'
+import cors from '@middy/http-cors'
 
 Sentry.AWSLambda.init({
     dsn: "https://fc657387286e49cb85c73e841794f225@o1071755.ingest.sentry.io/6071342",
     tracesSampleRate: 1.0,
-  });
+});
 
 const baseHandler = async (event) => {
     const id = event.queryStringParameters?.id // decoding already done
@@ -28,8 +28,11 @@ const baseHandler = async (event) => {
     return { statusCode: 200, body: JSON.stringify(result) }
 }
 
-export const handler = Sentry.AWSLambda.wrapHandler(baseHandler)
-// export const handler = middy(baseHandler)
-//     .use(errorLogger())
-//     .use(httpErrorHandler({ fallbackMessage: 'server error' }))
-//     .use(cors())
+// export const handler = Sentry.AWSLambda.wrapHandler(baseHandler)
+export const handler = Sentry.AWSLambda.wrapHandler(
+    middy(baseHandler)
+        .use(errorLogger())
+        .use(httpErrorHandler({ fallbackMessage: 'server error' }))
+        .use(cors())
+)
+// export const handler =
